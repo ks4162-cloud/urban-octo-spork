31136646a01bce80e0cd7a49a612ad646f8a1079
Developer <dev@example.com>
2026-03-11 14:21:28 +0530

    Add UC2: ASCII art banner for OOPS display

---
+++ step/UC2.java
@@ -0,0 +1,13 @@
+public class UC2 {
+    public static void main(String[] args) {
+        // Display "OOPS" as a 7-line ASCII art banner
+        
+        System.out.println(" ****  **   **  **** ");
+        System.out.println("*      * * * *  *    ");
+        System.out.println("*      *   * *  *    ");
+        System.out.println("*      * * * *  *    ");
+        System.out.println(" ****  **   **  **** ");
+        System.out.println("                     ");
+        System.out.println("                     ");
+    }
+}
